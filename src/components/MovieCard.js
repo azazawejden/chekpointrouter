5e90786e8ef1ff6c0dcc92a87d3847e04a06657f
azazawejden <azazawejden@gmail.com>
2023-04-13 00:50:32 +0100

    first commit

---
+++ src/components/MovieCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+function MovieCard({ title, description, posterURL, rating }) {
+    return (
+      <div>
+        <h2>{title}</h2>
+        <img src={posterURL} alt={title} />
+        <p>{description}</p>
+        <p>Rating: {rating}</p>
+      </div>
+    );
+  }
+  export default MovieCard;
