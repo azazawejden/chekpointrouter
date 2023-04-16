5ef879e29cc8384e7ab60c26f862223f37f3ddd1
azazawejden <azazawejden@gmail.com>
2023-04-17 00:22:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import MovieList from "./components/MovieList";
+import Filter from "./components/Filter ( title, rate)";
+
+const App = () => {
+  const [movies, setMovies] = useState([
+    {
+      title: "Final Fantasy XV",
+      description:
+        "Final Fantasy XV, annoncé originellement sous le titre Final Fantasy Versus XIII, est un jeu vidéo de la célèbre série Final Fantasy, développé et édité par Square Enix, sorti le 29 novembre 2016",
+      posterURL: "https://raw.githubusercontent.com/drminnaar/react-movie-cards/master/src/images/Kingsglaive_Final_Fantasy_XV.jpg",
+      rating: 9.2,
+      trailerLink: 'https://www.youtube.com/watch?v=Yi1_uZ3POXw',
+    },
+    {
+      title: "Final Fantasy Spirits Within",
+      description:
+        "C'est un film américano-japonais de Hironobu Sakaguchi (créateur de la série de jeux vidéo Final Fantasy) et Motonori Sakakibara, sorti le 15 août 2001. ",
+      posterURL: "https://raw.githubusercontent.com/drminnaar/react-movie-cards/master/src/images/Final_Fantasy_Spirits_Within.jpg",
+      rating: 9.3,
+      trailerLink: 'https://www.youtube.com/watch?v=wjTfS7SvlUE',
+    },
+  ]);
+
+  const [handleTitleChange, sethandleTitleChange] = useState("");
+  const [handleRatingChange, sethandleRatingChange] = useState("");
+  const [titleFilter, setTitleFilter] = useState("");
+  const [ratingFilter, setRatingFilter] = useState(0);
+
+  const handleTitleFilterChange = (event) => {
+    setTitleFilter(event.target.value);
+  };
+
+  const handleRatingFilterChange = (event) => {
+    setRatingFilter(Number(event.target.value));
+  };
+
+  const addMovie = (movie) => {
+    setMovies([...movies, movie]);
+  };
+
+  const filteredMovies = movies.filter((movie) => {
+    return (
+      movie.title.toLowerCase().includes(titleFilter.toLowerCase()) &&
+      movie.rating >= ratingFilter
+    );
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Movie App</h1>
+      <Filter
+        title={titleFilter}
+        rating={ratingFilter}
+        onTitleChange={handleTitleChange}
+        onRatingChange={handleRatingChange}
+      />
+      <MovieList movies={filteredMovies} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,11 +1,14 @@
 import React from "react";
-function MovieCard({ title, description, posterURL, rating }) {
+import { Link } from 'react-router-dom';
+function MovieCard({ title, description, posterURL, rating, movie }) {
     return (
       <div>
         <h2>{title}</h2>
         <img src={posterURL} alt={title} />
         <p>{description}</p>
         <p>Rating: {rating}</p>
+        <iframe src={movie.trailerLink} title={`${movie.title} Trailer`} width="560" height="315" frameBorder="0" allowFullScreen></iframe>
+      <Link to="/">Back to Home</Link>
       </div>
     );
   }
